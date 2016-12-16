cff1dfd1d7f0c72c73f27631e35732f103f9f810
master-alen <nchufujianjian@126.com>
2016-12-16 17:11:59 +0800

    optimize code - 2016-12-16

---
+++ subscription-utils/src/main/java/com/nchu/weixin/subscription/utils/JedisUtil.java
@@ -0,0 +1,188 @@
+package com.nchu.weixin.subscription.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * java redis 工具类
+ * Created by fujianjian on 2016/12/15.
+ */
+@Component
+@Slf4j
+public class JedisUtil {
+
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+    private static final String CHAR_ENCODING = "UTF-8";
+
+    private static final String GOODS_SERIAL_NO_KEY = "CURRENT_GOODS_SERIAL_NO_VALUE";
+
+    private static final String ORDER_SERIAL_NO_KEY = "CURRENT_ORDERE_SERIAL_NO_VALUE";
+
+    /**
+     * 获取redis键值对
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    public String get(String key) throws Exception {
+        RedisConnection connection = redisConnectionFactory.getConnection();
+        byte[] keyByte = key.getBytes(CHAR_ENCODING);
+        try {
+            byte[] bytes = connection.get(keyByte);
+            if(bytes == null){
+                return null;
+            }
+            return new String(bytes, CHAR_ENCODING);
+        } catch (Exception e) {
+            log.warn("redis 获取数据出现情况，现发起一次重复连接", e);
+            if (connection != null) {
+                connection.close();
+            }
+            connection = redisConnectionFactory.getConnection();
+            byte[] bytes = connection.get(keyByte);
+            if(bytes == null){
+                return null;
+            }
+            return new String(bytes, CHAR_ENCODING);
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    /**
+     * 设置redis键值对
+     * @param key
+     * @param value
+     * @param cacheSeconds 如果为0，则永久不过期，否则n秒后过期
+     * @throws Exception
+     */
+    public void set(String key, String value, int cacheSeconds) throws Exception {
+        RedisConnection connection = redisConnectionFactory.getConnection();
+        byte[] keyByte = key.getBytes("UTF-8");
+        byte[] valueByte = value.getBytes(CHAR_ENCODING);
+        try {
+            connection.set(keyByte, valueByte);
+            if (cacheSeconds != 0) {
+                connection.expire(keyByte, cacheSeconds);
+            }
+        } catch (Exception e) {
+            log.warn("redis 保存数据出现情况，现发起一次重复连接", e);
+            if (connection != null) {
+                connection.close();
+            }
+            connection = redisConnectionFactory.getConnection();
+            connection.set(keyByte, valueByte);
+            if (cacheSeconds != 0) {
+                connection.expire(keyByte, cacheSeconds);
+            }
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    /**
+     * 删除
+     * @param keys
+     * @return
+     * @throws Exception
+     */
+    public long del(String ...keys) throws Exception {
+        RedisConnection connection = redisConnectionFactory.getConnection();
+        byte[][] keyBytes= new byte[keys.length][];
+        for(int i=0;i<keys.length;i++){
+            keyBytes[i] = keys[i].getBytes(CHAR_ENCODING);
+        }
+        try {
+            return connection.del(keyBytes);
+        } catch (Exception e) {
+            log.warn("redis 获取数据出现情况，现发起一次重复连接", e);
+            if (connection != null) {
+                connection.close();
+            }
+            connection = redisConnectionFactory.getConnection();
+            return connection.del(keyBytes);
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    /**
+     * 自增长+1
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    public long incr(String key) throws Exception {
+        RedisConnection connection = redisConnectionFactory.getConnection();
+        byte[] keyByte = key.getBytes(CHAR_ENCODING);
+        try {
+            return connection.incr(keyByte);
+        } catch (Exception e) {
+            log.warn("redis 获取数据出现情况，现发起一次重复连接", e);
+            if (connection != null) {
+                connection.close();
+            }
+            connection = redisConnectionFactory.getConnection();
+            return connection.incr(keyByte);
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    /**
+     * 设置过期时间
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    public void expire(String key, int cacheSeconds) throws Exception {
+        RedisConnection connection = redisConnectionFactory.getConnection();
+        byte[] keyByte = key.getBytes(CHAR_ENCODING);
+        try {
+            if (cacheSeconds != 0) {
+                connection.expire(keyByte, cacheSeconds);
+            }
+        } catch (Exception e) {
+            log.warn("redis 获取数据出现情况，现发起一次重复连接", e);
+            if (connection != null) {
+                connection.close();
+            }
+            connection = redisConnectionFactory.getConnection();
+            if (cacheSeconds != 0) {
+                connection.expire(keyByte, cacheSeconds);
+            }
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    public boolean tryLock(String key) throws Exception{
+        long incr = incr(key);
+        if(incr == 1){
+            expire(key, 10);//10秒后自动释放锁，防止死锁
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void releaseLock(String key) throws Exception{
+        set(key, "0", 0);
+    }
+}
+}
--- subscription-web/src/main/java/com/nchu/weixin/subscription/action/manage/GoodsManageAction.java
+++ subscription-web/src/main/java/com/nchu/weixin/subscription/action/manage/GoodsManageAction.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -55,8 +56,13 @@ public class GoodsManageAction {
         return "manage/goods/create";
     }
 
+    /**
+     * 创建一个商品信息
+     * @param goods
+     * @return
+     */
     @RequestMapping(value = "/create", method = {RequestMethod.POST})
-    public String handleAdd(Goods goods){
+    public String handleAdd(@RequestParam MultipartFile iconImg, Goods goods){
         goods.setStatus(GoodsStatusEnum.WAIT_SHELVE);
         goodsService.create(goods);
         return "redirect:manage/goods/list";
