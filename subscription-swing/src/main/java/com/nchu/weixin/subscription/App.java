585a5882bf008434cc2dfa553f58ff5f35a5e5b3
fujianjian <nchufujianjian@sina.cn>
2017-01-24 13:54:32 +0800

    check in  code - 2017-01-24

---
+++ subscription-swing/src/main/java/com/nchu/weixin/subscription/App.java
@@ -0,0 +1,13 @@
+package com.nchu.weixin.subscription;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
